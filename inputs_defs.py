41250261ed1dfed91c8bf0aeccf0d88f0b9f0e18
avshalomn <46435789+avshalomn@users.noreply.github.com>
2019-12-04 14:28:46 +0200

    Update inputs_defs.py

--- inputs_defs.py
+++ inputs_defs.py
@@ -8,7 +8,7 @@ def random_inputs(shape,make_complex = 0):
     inps = np.array([[np.random.random() + 0.00000001j for c in range(cols)] for r in range(rows)])
   return inps
 
-def linear_targets(shape,make_complex = 0)
+def linear_targets(shape,make_complex = 0):
   rows, cols = shape[0],shape[1]
   targets = np.array([np.arange(cols)/cols + (make_complex*0.0000001j) for r in range(rows)])
      
