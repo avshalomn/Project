f446a42df4f9b684ce358f39b8a3e093bb79640d
avshalomn <46435789+avshalomn@users.noreply.github.com>
2019-12-04 12:06:22 +0200

    Update my_layers.py

--- my_layers.py
+++ my_layers.py
@@ -1,4 +1,13 @@
-class two2layer():
-  def __init__():
+import tensorflow as tf
+
+class two2layer(tf.keras.layers.Layer):
+  def __init__(self,shape,units = None):
+    super(two2layer,self).__init__()
+    self.shape = shape
+    self.units = shape[0] * shape[1]
+    
+  def get_config():
+    return super().get_config()
+    
   
  
