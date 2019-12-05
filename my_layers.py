18347ab24649d28a52ab421abcc168f85f181abd
avshalomn <46435789+avshalomn@users.noreply.github.com>
2019-12-05 15:15:13 +0200

    Update my_layers.py

--- my_layers.py
+++ my_layers.py
@@ -11,7 +11,8 @@ class layer2D(tf.keras.layers.Layer):
   def get_config():
     return super().get_config()
     
-    
+################################
+##  double weights layer ##
 ################################    
 class complex_weights_layer(tf.keras.layers.Layer)
   def __init__(self,shape,units=None):
@@ -22,11 +23,28 @@ class complex_weights_layer(tf.keras.layers.Layer)
     self.a = tf.Variable(tf.math.real(self.W), trainable = True)
     self.b = tf.Variable(tf.math.imag(self.W), trainable = True)
     
-    
-  def get_config():
+  def get_config(self):
     return super().get_config()
  
-  def make_tensor_weights():
+
+  def call(self,inputs):
+    
+    i_a = tf.math.real(inputs)
+    i_b = tf.math.imag(inputs)
+    # inputs amp and phase #
+    i_amp = tf.math.sqrt(i_a**2 + i_b**2)  # inputs amp
+    i_phase = tf.math.atan2(i_b,i_a)
+    # weights amp and phase #
+    w_amp = tf.math.sqrt(self.a**2 + self.b**2)
+    w_phase = tf.math.atan2(self.b,self.a)
+    
+    
+    
+    
+    return
+  
+
+  def make_tensor_weights(self):
     def make_a_weight():
       a = b = 1
       while cmath.polar(a + 1j*b)[0] >= 1:
